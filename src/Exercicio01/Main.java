d229297a803958ec9b2cdfd05b9f42e74a2712e3
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-20 17:30:25 -0300

    alterações exercicio 01

--- src/Exercicio01/Main.java
+++ src/Exercicio01/Main.java
@@ -3,23 +3,18 @@ package Exercicio01;
 public class Main {
 
     public static void main(String[] args) {
-	Carro carro1 = new Carro("Fusca", 2000, "Volkswagen", "Vermelha", 40000);
-	Carro carro2 = new Carro("Opala", 1500, "Chevrolet", "Preto", 53235);
+        Carro carro1 = new Carro("Fusca", 2000, "Volkswagen", "Vermelha", 40000);
+        Carro carro2 = new Carro("Opala", 1500, "Chevrolet", "Preto", 53235);
 
-	carro1.exibeDadosCarro();
-	carro1.descontadoDado(2569);
-		System.out.println("_____________");
-		carro1.exibeDadosCarro();
+        System.out.println("_____________");
+        carro1.exibeDadosCarro();
+        carro1.descontadoDado(2569);
 
-		// Testando os métodos criados
-		System.out.println("_____________");
-		carro1.pintura("Azul");
+        System.out.println("_____________");
+        carro1.pintura("Azul");
 
-		// Testando os métodos criados
-		System.out.println("_____________");
-		carro2.exibeDadosCarro();
-		carro2.descontadoDado(2000);
-
-
+        System.out.println("_____________");
+        carro2.exibeDadosCarro();
+        carro2.descontadoDado(2000);
     }
 }
