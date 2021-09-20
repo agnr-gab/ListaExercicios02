08ba036eca0d46ffb13fcb3122e0fbf1ce1e78ce
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-20 18:30:50 -0300

    realizando exercicio 02

--- src/Exercicio02/ContaBancaria.java
+++ src/Exercicio02/ContaBancaria.java
@@ -1,4 +1,5 @@
 package Exercicio02;
+
 /*
 2. Crie uma classe referente à uma conta bancária, que possui como atributos o seu
 número, saldo e o nome do correntista. Crie também o método saque() que serve
@@ -8,4 +9,29 @@ correntista, o número da conta e o saldo. Crie um programa principal e demonstr
 utilização dos métodos implementados.
  */
 public class ContaBancaria {
+    double numeroConta;
+    double saldoConta;
+    String nomeCliente;
+
+    public ContaBancaria(double numeroConta, double saldoConta, String nomeCliente) {
+        this.numeroConta = numeroConta;
+        this.saldoConta = saldoConta;
+        this.nomeCliente = nomeCliente;
+    }
+
+    public void saque(double valorSaque) {
+        saldoConta = saldoConta - valorSaque;
+
+    }
+
+    public void deposito(double valorDeposito) {
+        saldoConta = saldoConta + valorDeposito;
+    }
+
+    public void extrato() {
+        System.out.println("Cliente: " + nomeCliente);
+        System.out.println("Conta Corrente: " + numeroConta);
+        System.out.println("Saldo disponível: " + saldoConta);
+
+    }
 }
--- src/Exercicio02/Main.java
+++ src/Exercicio02/Main.java
@@ -2,6 +2,13 @@ package Exercicio02;
 
 public class Main {
     public static void main(String[] args) {
-
+        ContaBancaria correntista1 = new ContaBancaria(12319, 3698, "Maria J");
+        ContaBancaria correntista2 = new ContaBancaria(78897, 985, "Joana Maria Flor");
+        ContaBancaria correntista3 = new ContaBancaria(96321,100, "Paulo Alfredo Maques");
+        correntista1.extrato();
+        System.out.println("_____________");
+        correntista2.extrato();
+        System.out.println("_____________");
+        correntista3.extrato();
     }
 }
