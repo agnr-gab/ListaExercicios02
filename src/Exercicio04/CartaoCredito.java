cf2f027b00ba6c9abeb0b4862e7acafd4b0a3369
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-20 22:48:23 -0300

    adicionando metodos e objetos exercicio 04

--- src/Exercicio04/CartaoCredito.java
+++ src/Exercicio04/CartaoCredito.java
@@ -1,4 +1,5 @@
 package Exercicio04;
+
 /*
 4. Crie uma classe que represente um cartão de crédito, que deve ter como
 atributos o seu limite, o saldo da fatura e nome do cliente. Crie também os métodos
@@ -11,23 +12,26 @@ public class CartaoCredito {
     double saldoFatura;
     String nomeCliente;
 
-    public CartaoCredito(double limiteCredito, double saldoFatura, String nomeCliente){
+    public CartaoCredito(double limiteCredito, double saldoFatura, String nomeCliente) {
         this.limiteCredito = limiteCredito;
         this.saldoFatura = saldoFatura;
         this.nomeCliente = nomeCliente;
     }
-    public void aumentaLimite(double maisLimite){
+
+    public void aumentaLimite(double maisLimite) {
         limiteCredito = limiteCredito + maisLimite;
+    }
 
+    public void diminuiLimite() {
 
     }
-    public  void diminuiLimite() {
 
-    }
     public void realizaCompra() {
 
     }
+
     public void imprimeFatura() {
+        System.out.println("Valor da fatura: R$" + saldoFatura);
 
     }
 
